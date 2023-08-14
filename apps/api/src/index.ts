cda71896cd736d29f9a0b4a8cad5357a81309051
Esteban Benjumea <benjumeac12gmail.com>
2023-08-14 16:35:14 -0500

    add [db start, user services to index api]

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response } from "express";
 import config from "./config/config";
+import startDBConnection from "./db";
+import { UserRouter } from "./routes";
 
 const app = express();
 const PORT = config.port;
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Api Lafyuu");
 });
+
+app.use("/users", UserRouter);
+
+startDBConnection();
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
